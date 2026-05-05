386efb8ba34b2eb5ef2c832b572f027b1c0b2e1a
Nicolas Bourret <boubzy48@gmail.com>
2026-05-05 13:07:02 +0200

    create basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,179 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Input({ label, id, value, onchange }) {
+  return (
+    <div className="input">
+      <label htmlFor="width">{label}</label>
+      <input
+        type="number"
+        id={id}
+        name={id}
+        value={value}
+        onChange={onchange}
+      />
+    </div>
+  );
+}
+
+function WidthCalculation({
+  sampleWidth,
+  stitchNumber,
+  humanWidth,
+  handleSampleWidthChange,
+  handleStitchNumberChange,
+  handleHumanWidthChange,
+}) {
+  return (
+    <>
+      <Input
+        label="Your sample width (cm):"
+        id="sampleWidth"
+        value={sampleWidth}
+        onchange={handleSampleWidthChange}
+      />
+      <Input
+        label="Number of stitches:"
+        id="sampleStitches"
+        value={stitchNumber}
+        onchange={handleStitchNumberChange}
+      />
+      <Input
+        label="Your width (cm):"
+        id="humanWidth"
+        value={humanWidth}
+        onchange={handleHumanWidthChange}
+      />
+    </>
+  );
+}
+
+function HeightCalculation({ humanHeight, handleHumanHeightChange }) {
+  return (
+    <>
+      <Input
+        label="Your height (cm):"
+        id="humanHeight"
+        value={humanHeight}
+        onchange={handleHumanHeightChange}
+      />
+    </>
+  );
+}
+
+function Calculation({ calculatePullWidth, calculatePullHeight }) {
+  const [sampleWidth, setSampleWidth] = useState("");
+  const [stitchNumber, setStitchNumber] = useState("");
+  const [humanWidth, setHumanWidth] = useState("");
+  const [humanHeight, setHumanHeight] = useState("");
+
+  function handleSampleWidthChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = Number(e.target.value);
+    setSampleWidth(value);
+  }
+
+  function handleStitchNumberChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = Number(e.target.value);
+    setStitchNumber(value);
+  }
+
+  function handleHumanWidthChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = Number(e.target.value);
+    setHumanWidth(value);
+  }
+
+  function handleHumanHeightChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = Number(e.target.value);
+    setHumanHeight(value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    calculatePullWidth(sampleWidth, stitchNumber, humanWidth);
+    calculatePullHeight(humanHeight);
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+      <section>
+        <h1>Pull Builder</h1>
+        <form>
+          <WidthCalculation
+            sampleWidth={sampleWidth}
+            stitchNumber={stitchNumber}
+            humanWidth={humanWidth}
+            handleSampleWidthChange={handleSampleWidthChange}
+            handleStitchNumberChange={handleStitchNumberChange}
+            handleHumanWidthChange={handleHumanWidthChange}
+          />
+          <HeightCalculation
+            humanHeight={humanHeight}
+            handleHumanHeightChange={handleHumanHeightChange}
+          />
+          <button type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
+        </form>
       </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+function Result({ pullWidth, pullHeight }) {
+  return (
+    <>
+      <section>
+        <div>
+          <h2>Pull Width</h2>
+          <p>Number of stitches: {pullWidth}</p>
+        </div>
+        <div>
+          <h2>Pull Height</h2>
+          <p>First step: {pullHeight.firstStep} cm</p>
+          <p>Second step: {pullHeight.secondStep} cm</p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+function PullBuilder() {
+  const [pullWidth, setPullWidth] = useState(0);
+  const [pullHeight, setPullHeight] = useState({ firstStep: 0, secondStep: 0 });
+
+  function calculatePullWidth(sampleWidth, stitchNumber, humanWidth) {
+    if (sampleWidth > 0 && stitchNumber > 0 && humanWidth > 0) {
+      const stichesForTenCm = Math.round((stitchNumber * 10) / sampleWidth);
+      const pullWidth = (humanWidth * stichesForTenCm) / 10;
+      setPullWidth(Math.round(pullWidth));
+    }
+  }
+
+  function calculatePullHeight(humanHeight) {
+    if (humanHeight > 0) {
+      const firstStep = Math.round(humanHeight / 1.6);
+      const secondStep = Math.round(humanHeight / 3);
+
+      setPullHeight({ firstStep, secondStep });
+    }
+  }
+  return (
+    <>
+      <Calculation
+        calculatePullWidth={calculatePullWidth}
+        calculatePullHeight={calculatePullHeight}
+      />
+      <Result pullWidth={pullWidth} pullHeight={pullHeight} />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <PullBuilder />
+    </div>
+  );
+}
+
+export default App;
